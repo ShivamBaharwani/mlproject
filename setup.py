a1ed8955058978d449749e5bee4705d415fe0c88
ShivamBaharwani <youremail@example.com>
2024-09-15 00:15:25 +0530

    first setup commit

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str) ->List[str]:
+    '''
+    this function will returns the list of requirements
+    '''
+    requirements = []
+    with open(file_path,'r')as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+        
+
+    return requirements
 
 
-
-
-
-
-
-
-
-
-setup{
+setup(
     name = 'mlproject',
-    version = '1.1',
+    version = '0.0.1',
     author = 'Shivam',
-    author_email = 'krish'
-}
+    author_email = 'baharwanishivam@gmail.com',
+    packages = find_packages(),
+    nstall_requires = get_requirements('requirements.txt'),
+)
